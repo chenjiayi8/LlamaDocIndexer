dffa86d2044d1ac03919093061725683944aace3
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-12-28 13:32:18 +0000

    Add setup.py file for LlamaDocIndexer

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="LlamaDocIndexer",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A tool to index and monitor changes in document directories",
+    author="Jiayi Chen",
+    author_email="chenjiayi_344@hotmail.com",
+    url="https://github.com/chenjiayi8/LlamaDocIndexer",
+)
