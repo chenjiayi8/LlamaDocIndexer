be7a8dbb0e8fe6e52180e6d2552806d9d450ce10
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-12-30 00:45:31 +0000

    Update version and add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    required = f.read().splitlines()
+
 setup(
     name="LlamaDocIndexer",
-    version="0.0.1",
+    version="0.0.4",
     packages=find_packages(),
     description="A tool to index and monitor changes in document directories",
     author="Jiayi Chen",
     author_email="chenjiayi_344@hotmail.com",
     url="https://github.com/chenjiayi8/LlamaDocIndexer",
+    install_requires=required,
 )
