8a67d8ffdd82d933c548064f47272ae266fc50a1
Jiayi Chen <chenjiayi_344@hotmail.com>
2024-02-27 21:02:02 +0000

    Add menu file if it doesn't exist

--- LlamaDocIndexer/indexer.py
+++ LlamaDocIndexer/indexer.py
@@ -50,6 +50,9 @@ class Indexer:
                 self.indices["menu"] = json.load(f)
         else:
             self.indices["menu"] = {}
+            with open(menu_path, "w", encoding="utf-8") as f:
+                json.dump(self.indices["menu"], f, indent=4)
+
         self.load_indices()
 
     def load_indices(self):
