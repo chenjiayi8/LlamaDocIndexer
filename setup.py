f27b9fde29dd4785676d50c944d59eb9e9d0fa11
Jiayi Chen <chenjiayi_344@hotmail.com>
2024-03-19 14:23:43 +0000

    Add long description content type and download URL to setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     version="0.2.0",
     packages=find_packages(),
     description="A tool to index and monitor changes in document directories",
+    long_description_content_type="text/markdown",
     author="Jiayi Chen",
     author_email="chenjiayi_344@hotmail.com",
     url="https://github.com/chenjiayi8/LlamaDocIndexer",
+    download_url="https://pypi.org/project/LlamaDocIndexer/",
     install_requires=required,
 )
